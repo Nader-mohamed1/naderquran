86f6eb0835bf3a236e21f09b44b0cdc09b91911d
Nader <nm845313@gmail.com>
2024-12-20 16:28:06 +0200

    init project.

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatelessWidge {
+  static const String routName = 'Home';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('QUrane'),
+      ),
+    );
+  }
+}
